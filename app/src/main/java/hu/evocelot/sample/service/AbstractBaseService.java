6ce7493b455e4a65b40ef0bb5c70991d23678cd6
DanisovszkyMark <dmarkprog@gmail.com>
2025-01-04 16:46:05 +0100

    Sample endpoints

--- app/src/main/java/hu/evocelot/sample/service/AbstractBaseService.java
+++ app/src/main/java/hu/evocelot/sample/service/AbstractBaseService.java
@@ -2,6 +2,7 @@ package hu.evocelot.sample.service;
 
 import java.math.BigInteger;
 import java.time.OffsetDateTime;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,7 +13,7 @@ import hu.evocelot.sample.model.AbstractIdentifiedAuditEntity;
  * 
  * @author mark.danisovszky
  */
-public abstract class AbstractBaseService {
+public abstract class AbstractBaseService<T extends AbstractIdentifiedAuditEntity> {
 
     /**
      * Defines the JpaRepository to use when managing the entities.
@@ -27,11 +28,39 @@ public abstract class AbstractBaseService {
      * @param entity - the entity to save or update.
      * @return - with the updated entity.
      */
-    public AbstractIdentifiedAuditEntity save(AbstractIdentifiedAuditEntity entity) {
+    public T save(T entity) {
         entity.setVersion(entity.getVersion().add(BigInteger.valueOf(1)));
         entity.setModDate(OffsetDateTime.now());
         entity.setModUser("unknown"); // TODO: Get current user.
 
-        return (AbstractIdentifiedAuditEntity) getRepository().save(entity);
+        return (T) getRepository().save(entity);
+    }
+
+    /**
+     * Finds the {@link T} based on the id.
+     * 
+     * @param id - the id of the entity to find.
+     * @return - with {@link Optional<AbstractIdentifiedAuditEntity>}.
+     */
+    public Optional<T> findById(String id) {
+        return getRepository().findById(id);
+    }
+
+    /**
+     * Deletes the {@link T} based on the id.
+     * 
+     * @param id - the id of the entity to delete.
+     */
+    public void deleteById(String id) {
+        getRepository().delete(id);
+    }
+
+    /**
+     * Deletes the {@link T} entity.
+     * 
+     * @param entity - the entity to delete.
+     */
+    public void delete(T entity) {
+        getRepository().delete(entity);
     }
 }
